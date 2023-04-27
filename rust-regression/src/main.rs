3f434d09ec9cc4648d191919ca0ba0a07191b173
Vivian Wang <58998109+Mushroom-Wang@users.noreply.github.com>
2023-04-27 18:27:34 +0000

    upload dataset

--- rust-regression/src/main.rs
+++ rust-regression/src/main.rs
@@ -1,3 +1,13 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+use std::env::args;
+use std::vec::Vec;
+
+mod linear_regression;
+
 fn main() {
     println!("Hello, world!");
+    linear_regression::run();
 }
